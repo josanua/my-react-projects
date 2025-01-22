6398157bb5db43b872303a96468235c1902a344e
Andrew <thrueword@gmail.com>
2025-01-22 08:42:06 +0200

    Add React Essentials app with core concepts and examples
    Initialize a React app demonstrating core React concepts like Components, JSX, Props, and State. This includes assets, data, styled components, and interactive examples to enhance learning. Boilerplate setup with `App.js`, CSS, and supporting files is also included.

---
+++ udemy-react-essentials-app/src/App.js
@@ -0,0 +1,76 @@
+import { CORE_CONCEPTS, CORE_CONCEPTS as data, EXAMPLES } from "./data";
+import Header from "./components/header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TapButton";
+import { useState } from "react";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedBtn) {
+    setSelectedTopic(selectedBtn);
+    console.log(selectedBtn);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept key={item.title} {...item} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
