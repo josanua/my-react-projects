4d92c7f94b20176621351daef560560ac5ec7ee9
Andrew <thrueword@gmail.com>
2025-01-22 09:50:58 +0200

    Refactor: Extract Examples section into a separate component
    Moved the Examples section from App.js to a new Examples.jsx file for better modularity and maintainability.

--- udemy-react-essentials-app/src/App.js
+++ udemy-react-essentials-app/src/App.js
@@ -1,66 +1,14 @@
-import { EXAMPLES } from "./data";
 import Header from "./components/header/Header";
-import TabButton from "./components/TapButton";
-import { useState } from "react";
 import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedBtn) {
-    setSelectedTopic(selectedBtn);
-    console.log(selectedBtn);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
     <div>
       <Header />
       <main>
         <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <Examples/>
       </main>
     </div>
   );
