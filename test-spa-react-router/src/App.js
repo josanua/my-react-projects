9add6f2c023205cd61f26dee96259c194e684950
Andrew <thrueword@gmail.com>
2025-01-29 10:52:17 +0200

    Initialize React SPA with React Router
    Set up a new Single Page Application using Create React App. Added initial files.

---
+++ test-spa-react-router/src/App.js
@@ -0,0 +1,13 @@
+import {createBrowserRouter} from "react-router-dom";
+
+createBrowserRouter([
+  { path: '/', component: App},
+  {}
+])
+
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
