73701f4d3ef8137392aacd2b0ed021b2ed7582e7
Andrew <thrueword@gmail.com>
2025-01-29 14:49:11 +0200

    Refactor layout to include CSS modules for styling
    Added CSS modules to improve structure and maintainability of styles in `MainNavigation` and `RootLayout` components. Updated markup to leverage modular class names for better styling encapsulation and consistency.

--- test-spa-react-router/src/pages/Root.js
+++ test-spa-react-router/src/pages/Root.js
@@ -1,11 +1,14 @@
 import {Outlet} from 'react-router-dom'
 import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css"
 
 export default function RootLayout() {
     return (
         <>
             <MainNavigation/>
-            <Outlet/>
+            <main className={classes.content}>
+                <Outlet/>
+            </main>
         </>
     )
 }
