7825da440d17dda37945c913dca03a2816c2d768
Andrew <thrueword@gmail.com>
2025-01-30 19:18:02 +0200

    Add product details page and route handling
    Introduced a `ProductDetailPage` component to display individual product details using route parameters. Updated the `/products/:productId` route in `App.js` and enhanced the `Products` page to include a list of products. These changes improve navigation and content organization within the application.

--- test-spa-react-router/src/App.js
+++ test-spa-react-router/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 // JSX routes definition approach
 // const routeDefinitions = createRoutesFromElements(
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
         children: [
             {path: '/', element: <Home/>},
             {path: '/products', element: <Products/>},
+            {path: '/products/:productId', element: <ProductDetailPage/>},
         ],
     },
 ])
