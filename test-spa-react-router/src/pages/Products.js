09deb6ef4b6cb6be64f8949ff386d47f3103b2f6
Andrew <thrueword@gmail.com>
2025-01-30 19:21:17 +0200

    Fix data values.

--- test-spa-react-router/src/pages/Products.js
+++ test-spa-react-router/src/pages/Products.js
@@ -1,10 +1,10 @@
 import {Link} from 'react-router-dom';
 
 const PRODUCTS = [
-    {id: 1, name: 'Product 1'},
-    {id: 2, name: 'Product 2'},
-    {id: 3, name: 'Product 3'},
-    {id: 4, name: 'Product 4'},
+    {id: 'p1', name: 'Product 1'},
+    {id: 'p2', name: 'Product 2'},
+    {id: 'p3', name: 'Product 3'},
+    {id: 'p4', name: 'Product 4'},
 ]
 
 export default function Products() {
